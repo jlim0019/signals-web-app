53ad5ca2a678c9f8b62fa4d8f5b3b113686d67c9
Jason Lim <you@example.com>
2020-08-05 23:52:56 +1000

    Updates

---
+++ precache-manifest.625d3528e1bbe900a89aa031b35dce31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0194509aafa14e0c84fd5bdacb46eaa0",
+    "url": "/signals-web-app/index.html"
+  },
+  {
+    "revision": "b9f3afe99ab3b3197229",
+    "url": "/signals-web-app/static/css/main.2e2883be.chunk.css"
+  },
+  {
+    "revision": "44dd03a4b228de2289fa",
+    "url": "/signals-web-app/static/js/2.6209a42d.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/signals-web-app/static/js/2.6209a42d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9f3afe99ab3b3197229",
+    "url": "/signals-web-app/static/js/main.e25fd148.chunk.js"
+  },
+  {
+    "revision": "652c5d2703387bf2fef3",
+    "url": "/signals-web-app/static/js/runtime-main.c911726b.js"
+  }
+]);

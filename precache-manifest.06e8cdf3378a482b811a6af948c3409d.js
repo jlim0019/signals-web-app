fa06329fae6a8443a13bb8f6686925beba78f757
Jason Lim <you@example.com>
2020-10-28 23:59:46 +1100

    Updates

--- precache-manifest.06e8cdf3378a482b811a6af948c3409d.js
+++ precache-manifest.06e8cdf3378a482b811a6af948c3409d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "979ff13879e4be34fa517776a18c3e01",
+    "revision": "8ea4de30c599d11c28276a9165eccaa8",
     "url": "/signals-web-app/index.html"
   },
   {
-    "revision": "c8e7ea2153ed9577ce56",
+    "revision": "d65579a701e2a626c70e",
     "url": "/signals-web-app/static/css/main.688fcd95.chunk.css"
   },
   {
-    "revision": "f9ead9223aaf061441a4",
-    "url": "/signals-web-app/static/js/2.1152f352.chunk.js"
+    "revision": "dd88ad3aaa38a85f45d1",
+    "url": "/signals-web-app/static/js/2.fab60000.chunk.js"
   },
   {
     "revision": "c0e192ee1efd8e581f30f464cbdc5508",
-    "url": "/signals-web-app/static/js/2.1152f352.chunk.js.LICENSE.txt"
+    "url": "/signals-web-app/static/js/2.fab60000.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c8e7ea2153ed9577ce56",
-    "url": "/signals-web-app/static/js/main.98aad27a.chunk.js"
+    "revision": "d65579a701e2a626c70e",
+    "url": "/signals-web-app/static/js/main.a299b96b.chunk.js"
   },
   {
     "revision": "652c5d2703387bf2fef3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/signals-web-app/precache-manifest.78228ed08e560151feb052b5373d5ff7.js"
+  "/signals-web-app/precache-manifest.06e8cdf3378a482b811a6af948c3409d.js"
 );
 
 self.addEventListener('message', (event) => {
